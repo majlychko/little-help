28c932fc08f4b707d159c0f0d8a22bafd7a06947
Анастасия <65824083+majlychko@users.noreply.github.com>
2024-01-11 18:00:14 +0300

    Update unbreakables.py

--- unbreakables.py
+++ unbreakables.py
@@ -2,19 +2,16 @@ import json
 import re
 
 def replace_space(text):
-    # ищем слова из 1 или 2 символов за которыми пробел рег выражением,
+    # используем регулярное выражение для поиска слов из 1 или 2 символов,
     pattern = r'(\b\w{1,2}\b) '
     result = re.sub(pattern, r'\1\\u00A0', text)
     
-    # ниже всякие палочки 
     # Заменяем пробелы на неразрывные, если после пробела идет длинное тире
     result = re.sub(r' —', r'\\u00A0—', result)
     # Заменяем пробелы на неразрывные, если после пробела идет тире со стрелочкой
     result = re.sub(r' →', r'\\u00A0→', result)
-    # Заменяем короткое тире на неразнывное
-    result = re.sub(r'-', r'\\u2011', result)
 
-    #ниже идут валюты и цифры и все что с ними связано
+    #ниже идут валюты и цифры
     # Заменяем пробелы на неразрывные, до знака рубль
     result = re.sub(r' ₽', r'\\u00A0₽', result)
     # Заменяем пробелы на неразрывные, до слова рублей
@@ -34,14 +31,48 @@ def replace_space(text):
     # Заменяем пробелы на неразрывные, до млрд 
     result = re.sub(r' млрд', r'\\u00A0млрд', result)
 
+    # Заменяем короткое тире на неразнывное
+    result = re.sub(r'-', r'\\u2011', result)
+
     # Заменяем т-ж на т-ж с неразрывными пробелами нулевой ширины
     result = re.sub(r'Т—Ж', r'Т\\u2060—\\u2060Ж', result)
 
-    # продукты тинька, редактор сказал ставить в них неразрывники
-    result = re.sub(r'Tinkoff ', r'Tinkoff\\u00A0', result)
-    result = re.sub(r'Тинькофф ', r'Тинькофф\\u00A0', result)
-    result = re.sub(r'ALL ', r'ALL\\u00A0', result)
+    #ниже идут продукты тинька, Артур сказал неразрывники ставить в них
+    #увы не можем просто после слова тинькофф ставить неразрывник, ибо оно не всегда с продутом идет
+    #result = re.sub(r'Tinkoff ', r'Tinkoff\\u00A0', result)
+    #result = re.sub(r'Тинькофф ', r'Тинькофф\\u00A0', result)
+    #result = re.sub(r'ALL ', r'ALL\\u00A0', result)
 
+    # Заменяем Tinkoff Pro на Tinkoff неразрывник Pro
+    result = re.sub(r'Tinkoff Pro', r'Tinkoff\\u00A0Pro', result)
+    # Заменяем Tinkoff Premium на Tinkoff неразрывник Premium
+    result = re.sub(r'Tinkoff Premium', r'Tinkoff\\u00A0Premium', result)
+    # Заменяем Tinkoff Private на Tinkoff неразрывник Private
+    result = re.sub(r'Tinkoff Private', r'Tinkoff\\u00A0Private', result)
+    # Заменяем Tinkoff Black на Tinkoff неразрывник Black
+    result = re.sub(r'Tinkoff Black', r'Tinkoff\\u00A0Black', result)
+    # Заменяем Tinkoff ID на Tinkoff неразрывник ID
+    result = re.sub(r'Tinkoff ID', r'Tinkoff\\u00A0ID', result)
+    # Заменяем Tinkoff Pay на Tinkoff неразрывник Pay
+    result = re.sub(r'Tinkoff Pay', r'Tinkoff\\u00A0Pay', result)
+    # Заменяем Тинькофф Платинум на Тинькофф неразрывник Платинум
+    result = re.sub(r'Тинькофф Платинум', r'Тинькофф\\u00A0Платинум', result)
+    # Заменяем Тинькофф Сторис на Тинькофф неразрывник Сторис
+    result = re.sub(r'Тинькофф Сторис', r'Тинькофф\\u00A0Сторис', result)
+    # Заменяем Тинькофф Страхование на Тинькофф неразрывник Страхование
+    result = re.sub(r'Тинькофф Страхование', r'Тинькофф\\u00A0Страхование', result)
+    # Заменяем Тинькофф Бизнес на Тинькофф неразрывник Бизнес
+    result = re.sub(r'Тинькофф Бизнес', r'Тинькофф\\u00A0Бизнес', result)
+    # Заменяем Тинькофф Путешествия на Тинькофф неразрывник Путешествия
+    result = re.sub(r'Тинькофф Путешествия', r'Тинькофф\\u00A0Путешествия', result)
+    # Заменяем Тинькофф Квест на Тинькофф неразрывник Квест
+    result = re.sub(r'Тинькофф Квест', r'Тинькофф\\u00A0Квест', result)
+    # Заменяем Тинькофф Банк на Тинькофф неразрывник Банк
+    result = re.sub(r'Тинькофф Банк', r'Тинькофф\\u00A0Банк', result)
+    # Заменяем Тинькофф Мобайл на Тинькофф неразрывник Мобайл
+    result = re.sub(r'Тинькофф Мобайл', r'Тинькофф\\u00A0Мобайл', result)
+    # Заменяем Тинькофф Инвестиции на Тинькофф неразрывник Инвестиции
+    result = re.sub(r'Тинькофф Инвестиции', r'Тинькофф\\u00A0Инвестиции', result)
     return result
 
 # при помощи рекурсии в которой находим с json файле ключи, в которых необходимо заменить пробелы
@@ -62,6 +93,7 @@ with open('story.json', 'r') as json_file:
 
 # вызываем функцию для замены значений тегов text_with_tags и name (печенька) + subtitlе (перслента) + alternative_title (перслента заг)
 replace_text_by_tag(data)
+
 
 # сохраняем измененные данные json в новый файл
 with open('updated_story.json', 'w') as updated_file:
